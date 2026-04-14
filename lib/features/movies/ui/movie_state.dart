3b95810d3bbcc7238d8eb403119372ce65e41b29
Ahmed Hytham Ahmed Samir <ahmedhytham48@gmail.com>
2026-04-15 01:52:45 +0200

    - Integrating API "TMDB" with: 1- Freezed: to generates data models with built-in JSON parsing and "union" states. 2- Dio: because its a powerful HTTP client for Dart that handles our TMDB API calls. 3- State Management with Bloc/Cubit	: because it manages the UI lifecycle (Loading → Success → Error). in view of Clean Architecture.

---
+++ lib/features/movies/ui/movie_state.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import '../data/models/movie_model.dart';
+
+part 'movie_state.freezed.dart';
+
+@freezed
+class MovieState with _$MovieState {
+  const factory MovieState.initial() = _Initial;
+  const factory MovieState.loading() = _Loading;
+  const factory MovieState.success(List<Movie> movies) = _Success;
+  const factory MovieState.error(String message) = _Error;
+}
