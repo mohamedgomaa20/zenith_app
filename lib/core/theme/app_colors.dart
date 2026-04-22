addedcf6f171e4c603e527ca95a034e55e7a8c54
youssef hamada <youssef.hamada0101@gmail.com>
2026-04-22 04:29:05 +0200

    add some colors

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -6,7 +6,17 @@ class AppColors {
   static const Color darkSurface = Color(0xFF232D48);
   static const Color darkText = Colors.white;
   static const Color darkSubText = Color(0xFF98A2B3);
-
+  static const Color background   = Color(0xFF0A0A0F);
+  static const Color surface      = Color(0xFF111118);
+  static const Color surface2     = Color(0xFF1A1A24);
+  static const Color surface3     = Color(0xFF22222F);
+  static const Color gold         = Color(0xFFF5C518);
+  static const Color goldDim      = Color(0xFFC9A014);
+  static const Color red          = Color(0xFFE5383B);
+  static const Color green        = Color(0xFF2DC653);
+  static const Color textPrimary  = Color(0xFFF0F0F0);
+  static const Color textSecondary= Color(0xFF888888);
+  static const Color textDim      = Color(0xFF555566);
   // Light Theme Colors
   static const Color lightBackground = Color(0xFFF7F7F7);
   static const Color lightSurface = Colors.white;
